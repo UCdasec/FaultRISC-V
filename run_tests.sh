a200bc1617495c6d72a0f02049fe1ef5f57349e5
kharanpv <kharanpv@mail.uc.edu>
2024-06-17 15:09:40 -0400

    Completed shell script. TODO: Make comparison script to compare and generate report against ground truth

--- run_tests.sh
+++ run_tests.sh
@@ -13,3 +13,12 @@ TIMESTAMP=$(date +"%Y-%m-%d_%H-%M")
 # Create the new directory with the current date and time as the name
 NEW_RESULT="$RESULTS_DIR/$TIMESTAMP.json"
 touch "$NEW_RESULT"
+
+# Get the full directory of the folder with the test set and make list of .s files in folder
+SOURCE_DIR=$(dirname "$(realpath "$1")")
+FILES=$(find $SOURCE_DIR -type f -name "*.s")
+
+# For each .s file, run the report
+for FILE in $FILES; do
+  python3 main.py $FILE --store_result yes --result_location report
+done
