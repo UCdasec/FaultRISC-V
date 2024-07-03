36743e43fed8d5890925bbd78cd93d573fe0813a
kharanpv <kharanpv@mail.uc.edu>
2024-07-03 15:26:18 -0400

    Modified testing bash script to accept predetermined optimization level for analysis

--- run_tests.sh
+++ run_tests.sh
@@ -14,13 +14,35 @@ TIMESTAMP=$(date +"%Y-%m-%d_%H-%M")
 NEW_RESULT="$REPORTS_DIR/Report_$TIMESTAMP.json"
 touch "$NEW_RESULT"
 
+
+# Optional Variable: Optimization level
+OPT_LEVEL = ""
+
+while getopts "o:" option; do
+  case "$option" in
+    o)
+      OPT_LEVEL=$OPTARG
+      ;;
+    *)
+      echo "Usage: ./run_tests.sh [-opt optimization_level(O0|O1|O2)] source_dir"
+      exit 1
+      ;;
+  esac
+done
+
+shift $((OPTIND-1))
+
 # Get the full directory of the folder with the test set and make list of .s files in folder
 SOURCE_DIR=$(realpath "$1")
 FILES=$(find "$SOURCE_DIR" -type f -name "*.s")
 
 # For each file, run the report
 for FILE in $FILES; do
-  python3 main.py "$FILE" --store_result report
+  if [ -n "$OPT_LEVEL" ]; then
+    python3 main.py "$FILE" --optimization_level "$OPT_LEVEL" --store_result report
+  else
+    python3 main.py "$FILE" --store_result report
+  fi
 done
 
 # Create the Analysis directory if it doesn't exist
